a6cc911eaa4e7ffd55d601b09a0c9e92aa24b901
Filip Moreau <filip.moreau@hotmail.com>
2021-05-17 12:30:19 +0200

    Speed and Turning can now be set externally [-1,0]. Sensor values now range from 0 to 1. Visual update

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -1,18 +1,35 @@
 using UnityEngine;
+using UnityEditor;
 
 [RequireComponent(typeof(Rigidbody))]
 public class CarController : MonoBehaviour
 {
-    private Transform[] sensors = new Transform[5];
+    private Transform[] sensors;
     private const float SENSOR_RANGE = 5.0f;
 
-    private const float MOVE_SPEED = 0.05f;
-    private const float MAX_TURN_SPEED = 1.0f;
+    private const float MAX_MOVE_SPEED = 0.05f;
+    private const float MAX_TURN_SPEED = 1.5f;
     private Rigidbody rb;
+
+
+    [SerializeField] [Range(-1.0f, 1.0f)] private float speed = 0.0f;    //The car's speed [0,1]
+    [SerializeField] [Range(-1.0f, 1.0f)] private float turning = 0.0f;  //The car's turning [-1,1]
+
+    /// <summary>
+    /// Property for setting this car's speed [0,1]
+    /// </summary>
+    public float Speed { set { speed = value; } }
+
+    /// <summary>
+    /// Property for setting this car's turning [-1,1]
+    /// </summary>
+    public float Turning { set { turning = value; } }
+
 
 
     private void Awake()
     {
+        sensors = new Transform[transform.childCount];
         for (int i = 0; i < sensors.Length; i++)
             sensors[i] = transform.GetChild(i);
 
@@ -20,39 +37,40 @@ public class CarController : MonoBehaviour
     }
 
 
-    public float[] SensorRanges()
+    public float[] GetSensorValues()
     {
-        float[] sensorRanges = new float[sensors.Length];
+        float[] sensorValues = new float[sensors.Length];
         for (int i = 0; i < sensors.Length; i++)
         {
             RaycastHit hit;
             if (Physics.Raycast(sensors[i].position, sensors[i].forward, out hit, SENSOR_RANGE))
             {
-                sensorRanges[i] = SENSOR_RANGE - hit.distance;
+                sensorValues[i] = 1.0f - hit.distance / SENSOR_RANGE;
             }
             else
             {
-                sensorRanges[i] = -1.0f;
+                sensorValues[i] = 0.0f;
             }
         }
 
-        return sensorRanges;
+        return sensorValues;
     }
 
 
     private void FixedUpdate()
     {
-        Move();
+        Move(speed);
+        Turn(turning);
     }
 
-    private void Move()
+    private void Move(float speed)
     {
-        rb.MovePosition(transform.position + transform.forward * MOVE_SPEED);
+        rb.MovePosition(transform.position + transform.forward * speed * MAX_MOVE_SPEED);
     }
 
     public void Turn(float turning)
     {
-        rb.MoveRotation(rb.rotation * Quaternion.AngleAxis(turning * MAX_TURN_SPEED, Vector3.up));
+        rb.MoveRotation(rb.rotation * Quaternion.AngleAxis(turning * MAX_TURN_SPEED * speed, Vector3.up));
     }
 
 
@@ -61,21 +79,23 @@ public class CarController : MonoBehaviour
     {
         try
         {
-            float[] sensorRanges = SensorRanges();
+            float[] sensorValues = GetSensorValues();
 
             for (int i = 0; i < sensors.Length; i++)
             {
-                if (sensorRanges[i] == -1.0f)
+                if (sensorValues[i] == 0.0f)
                 {
                     Gizmos.color = Color.green;
                 }
                 else
                 {
-                    Gizmos.color = new Color(1.0f, (SENSOR_RANGE - sensorRanges[i]) / (SENSOR_RANGE * 2.0f), 0.0f);
+                    Gizmos.color = new Color(1.0f, 1.0f - sensorValues[i], 0.0f);
                 }
 
-                float rayLength = (sensorRanges[i] == -1.0f) ? SENSOR_RANGE : SENSOR_RANGE - sensorRanges[i];
-                Gizmos.DrawLine(sensors[i].position, sensors[i].position + sensors[i].forward * rayLength);
+                Vector3 hitPoint = sensors[i].position + sensors[i].forward * (1.0f - sensorValues[i]) * SENSOR_RANGE;
+                Gizmos.DrawLine(sensors[i].position, hitPoint);
+
+                Handles.Label(hitPoint, sensorValues[i].ToString());
             }
         }
         catch (System.Exception) { }
